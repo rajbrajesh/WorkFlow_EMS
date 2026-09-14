e4fe9b5343934a0046d1de997ffe44478a4eede9
Brajesh Raj <aec.cse.rajbrajesh@gmail.com>
2026-09-14 14:28:10 +0530

    Add application role structure

---
+++ backend/workflow/src/main/java/com/workflow/workflow/entity/Role.java
@@ -0,0 +1,15 @@
+package com.workflow.workflow.entity;
+
+/**
+ * Defines the roles available in the WorkFlow application.
+ *
+ * ADMIN - Full access to the application.
+ * HR    - Employee management access.
+ * USER  - Basic authenticated user access.
+ */
+public enum Role {
+
+    ADMIN,
+    HR,
+    USER
+}
